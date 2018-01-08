29550e665408acd470c1b49bd5c790b16763b537
Lucas Matheus Testa <lucas@agrtec.com.br>
2018-01-08 15:10:18 -0200

    removed hasapiToken

--- app/User.php
+++ app/User.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use Laravel\Passport\HasApiTokens;
+//use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;//HasApiTokens, Notifiable;
 
     const ROLE_ADMIN = 'admin';
 
