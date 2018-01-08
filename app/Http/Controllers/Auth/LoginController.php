b8c13b9617f99630794a0f52fefff1cde3b89f42
Lucas Matheus Testa <lucas@agrtec.com.br>
2018-01-08 17:35:02 -0200

    request

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class LoginController extends sddsd
+class LoginController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
